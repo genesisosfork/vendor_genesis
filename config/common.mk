df9c6e98c254e3cbbc7cc4fd87a2e2fbb74d41bc
backslashxx <118538522+backslashxx@users.noreply.github.com>
2026-03-13 15:37:18 +0100

    common: remove addond updater

--- config/common.mk
+++ config/common.mk
@@ -33,11 +33,7 @@ endif
 # Backup Tool
 PRODUCT_COPY_FILES += \
     vendor/genesis/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
-    vendor/genesis/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
-    vendor/genesis/prebuilt/common/bin/50-genesis.sh:$(TARGET_COPY_OUT_SYSTEM)/addon.d/50-genesis.sh
-
-PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
-    system/addon.d/50-genesis.sh
+    vendor/genesis/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions
 
 ifneq ($(strip $(AB_OTA_PARTITIONS) $(AB_OTA_POSTINSTALL_CONFIG)),)
 PRODUCT_COPY_FILES += \
@@ -50,10 +46,8 @@ PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
     system/bin/backuptool_ab.functions \
     system/bin/backuptool_postinstall.sh
 
-ifneq ($(TARGET_BUILD_VARIANT),user)
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.ota.allow_downgrade=true
-endif
 endif
 
 # BtHelper
